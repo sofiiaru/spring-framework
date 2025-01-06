8aefff45ff166a0864b278fe22787db33676810a
sofiiaru <rudenka93@gmail.com>
2025-01-06 11:38:47 -0500

    mentor task

---
+++ 07-Spring-MVC-Controller/src/main/java/com/example/controller/MentorController.java
@@ -0,0 +1,25 @@
+package com.example.controller;
+
+import com.example.enums.Gender;
+import com.example.model.Mentor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class MentorController {
+    @RequestMapping
+    public String showTable(Model model) {
+        List<Mentor> mentorList = new ArrayList<>();
+        mentorList.add(new Mentor("Mike", "Smith", 45, Gender.MALE));
+        mentorList.add(new Mentor("Tom", "Hanks", 38, Gender.MALE));
+        mentorList.add(new Mentor("Ammy", "Bryan", 33, Gender.FEMALE));
+
+        model.addAttribute("mentors", mentorList);
+
+        return "mentor/mentor-list";
+    }
+}
